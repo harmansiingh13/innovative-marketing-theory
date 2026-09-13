6a0c9f75a53db49a6f12218be793ac695025107d
Harman Singh <hrmn.siingh@gmail.com>
2026-09-13 12:18:04 +0530

    fix smooth scroll issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+      data-scroll-behavior="smooth"
+    >
       <body>
         {children} <ToasterProvider />
       </body>
